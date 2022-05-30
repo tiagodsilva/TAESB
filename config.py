4ff4efa29dc19e10c212de0c4936be81f0d5e45a
tiagodsilva <tdsh97@gmail.com>
2022-05-30 00:23:33 -0300

    Insert configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+""" 
+Configurations for the world in which the ants' empire lives prosperously.
+""" 
+WIDTH = 9
+HTIGHT = 9
+ANTHILLS = [("Spartans", 1, 3, 15), ("Jedi", 5, 5, 99)]
+FOODS = [(1, 5, 9)]
+FOOD_UPDATE = 3
+ANTS_FOV = 1
+PHEROMONES_LIFETIME = 9
+MAX_FOODS = 1e14
--- main.py
+++ main.py
@@ -5,21 +5,86 @@ import os
 from taesb.Map import Map 
 
 import argparse 
+
+def anthills(s: str): 
+    """ 
+    Consolidate the data in anthills, which has the format 
+
+    name,x,y,ants;name,x,y,ants;...;name,x,y,ants 
+
+    It computes, then, 
+
+    [(name,x,y,ants),(name,x,y,ants),...,(name,x,y,ants)] 
+    """ 
+    try: 
+        # Split the data with the delimiter ";" 
+        csvs = s.split(";") # List of csvs 
+        
+        # Convert csv to a list of tuples 
+        s = [tuple(csv.split(",")) for csv in csvs] 
+        # and use an appropriate type 
+        s = [(str(name), int(x), int(y), int(ants)) for name, x, y, ants in \
+                s] 
+
+        return anthills 
+    except: 
+        raise argparse.ArgumentTypeError("The parameter `anthills` should have specifically \
+the format `name[str],x[int],y[int],ants[int];...;name[str],x[int],y[int],ants[int]`") 
+ 
+def foods(s: str): 
+    """ 
+    Convert a list of CSVs with the format 
+
+    x,y,volume;...;x,y,volume 
+
+    to a list of tuples with the format 
+
+    [(x,y,volume),...(x,y,volume)] 
+    """ 
+    try: 
+        # Split the data with ";" 
+        csvs = s.split(";") 
+        # and further split the csvs 
+        s = [tuple(csv.split(",")) for csv in csvs] 
+
+        # Cast the parameters 
+        s = [(int(x), int(y), int(volume)) for x, y, volume in s] 
+
+        return s 
+    except: 
+        raise argparse.ArgumentTypeError("The `foods` parameter should have the format \
+equivalent to x[int],y[int],volume[int];...;x[int],y[int],volume[int]") 
+
+def parse_args(parser: argparse.ArgumentParser): 
+    """ 
+    Parse the command line parameters for the parser `parser`. 
+    """ 
+    parser.add_argument("--width", help="The width of the map.", 
+            type=int) 
+    parser.add_argument("--height", help="The height of the map.", 
+            type=int) 
+    parser.add_argument("--anthills", help="The distributions of the anthills across the map, \
+with the format name[str],x[int],y[int],ants[int];...;name[str],x[int],y[int],ants[int].", 
+            type=anthills) 
+    parser.add_argument("--foods", help="The distributions of the foods across the map, \
+with the format x[int],y[int],volume[int];...;x[int],y[int],volume[int].") 
+    parser.add_argument("--food_update", help="The rate with which we update the foods in the map.", 
+            type=int) 
+    parser.add_argument("--ants_fov", help="The ants' field of view.", 
+            type=int) 
+    parser.add_argument("--pherlt", help="The pheromones' lifetime.", 
+            type=int) 
+    parser.add_argument("--verbose", help="Whether to display the map's characteristics.", 
+            type=bool, default=False) 
+    
+    return parser.parse_args() 
+
 def main(args): 
     """ 
     Initialize the simulation. 
     """ 
     # Instantiate a map 
-    width = 9
-    height = 9 
-    anthills = [("Spartans", 1, 3, 15), ("Jedi", 5, 5, 99)] 
-    foods = [(1, 5, 9)] 
-    food_update = 3 
-    ants_fov = 1 
-    pheromones_lifetime = 9
-    max_foods = 19 
-    verbose = True 
-    
+
     world = Map( 
             width=args.width, 
             height=args.height,
@@ -34,4 +99,6 @@ def main(args):
     world.run(max_foods=args.max_foods) 
 
 if __name__ == "__main__": 
-    main() 
+    parser = argparse.ArgumentParser(description="Parse the parameters to start the invasion of the ants.")
+    args = parse_args(parser) 
+    main(args) 
