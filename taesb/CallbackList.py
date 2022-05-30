db4d43e05bff34ccda6d213a292ec90ad62bd76e
tiagodsilva <tdsh97@gmail.com>
2022-05-30 09:42:31 -0300

    Write callbacks for Celery

---
+++ taesb/CallbackList.py
@@ -0,0 +1,32 @@
+""" 
+Implement a list of callbacks for the Celery tasks. 
+""" 
+# Celery 
+import celery 
+
+class CallbacksList(object): 
+    """ 
+    A list of callbacks for the Celery application; it tackles the transfer of data. 
+    """ 
+
+    def __init__(self, *args): 
+        """ 
+        Constructor method for `CallbacksList`. 
+
+        Parameters 
+        --------- 
+        args: celery.Task 
+            each component of `args' is a task for celery, which uses an Map 
+            as an parameter 
+        """ 
+        # List of the calbacks to be executed 
+        self.callbacks = list(args) 
+
+    def execute(self, global_map: 'Map'): # Forward reference 
+        """ 
+        Execute each callback. 
+        """ 
+        # Serialize map 
+        json_map = global_map.to_json() 
+        for callback in callbacks: 
+            callbacks.delay(json_map) 
