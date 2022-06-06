07c8d025c092f12f88eb0946d896b7fe7c788e9e
tiagodsilva <tdsh97@gmail.com>
2022-06-05 21:14:33 -0300

    Insert methods to capture data from the data set

--- taesb/utils/CelerySpark.py
+++ taesb/utils/CelerySpark.py
@@ -11,6 +11,8 @@ from celery import Celery
 import pyspark 
 from pyspark.sql import SparkSession 
 
+import psycopg2 
+
 class CelerySpark(Celery): 
     """ 
     A class that assembles Celery and Spark. 
@@ -26,28 +28,35 @@ class CelerySpark(Celery):
         # Attributes to combine data from the PostgreSQL data base 
         self.db_conn = None 
         self.spark_session = None 
-        
+        # The name of the database 
+        self.database = None 
+        self.db_url = None 
+
         # Insert the application name 
         self.app_name = kwargs["main"] 
 
-    def init_worker(database: str, user: str, password: str, 
-            queries: str, app_name: str): 
+    def _init_database(self, database: str, user: str, password: str, 
+            queries: str): 
         """ 
         Initialize the workers, with the Spark attributes. 
         """ 
-        self.db_conn = pycopg2.connect( 
+        self.db_conn = psycopg2.connect( 
                 database=database, 
                 user=user,
                 password=password 
         ) 
         
+        # Use data base attributes 
+        self.database = database 
+        self.db_url = "jdbc:postgresql://localhost:5432/{database}".format(database=database) 
+
         # Execute the queries 
-        execute_query("\n".join(queries)) 
+        self.execute_query("\n".join(queries)) 
 
         # Get (or create) the current Spark session 
         self.spark_session = SparkSession \
                 .builder \
-                .appName(self.app_name) 
+                .appName(self.app_name) \
                 .config("spark.jars", "postgresql-42.3.6.jar") \
                 .getOrCreate() 
 
@@ -55,7 +64,7 @@ class CelerySpark(Celery):
         sc = pyspark.SparkContext.getOrCreate() 
         sc.setLogLevel("FATAL") 
 
-    def execute_query(query: str): 
+    def execute_query(self, query: str): 
         """ 
         Execute a query in the data base pointed by `db_conn`. 
         """ 
@@ -66,5 +75,37 @@ class CelerySpark(Celery):
         # and commit the changes 
         cur.close() 
         self.db_conn.commit() 
+    
+    def read_table(self, tablename: str): 
+        """ 
+        Capture a data table from the database. 
+        """ 
+        # Query a table in the database 
+        rdd = self.spark_session \
+                .format("jdbc") \
+                .option("url", self.db_url) \
+                .optoin("table", tablename) \
+                .option("user", "tiago") \
+                .option("password", "password") \
+                .option("driver", "org.postgresql.Driver") \
+                .load() 
 
+        # Return the resilient and distributed data set
+        return rdd 
+    
+    def query_db(self, query: str): 
+        """ 
+        Capture a registers with a query in the database. 
+        """ 
+        rdd = self.spark_session \
+                .format("jdbc") \
+                .option("url", self.db_url) \
+                .optoin("query", "query") \
+                .option("user", "tiago") \
+                .option("password", "password") \
+                .option("driver", "org.postgresql.Driver") 
+                .load() 
+
+        # Return the resilient and distributed data set 
+        return rdd 
 
