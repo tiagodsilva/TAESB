8abcec38f531f3791a0aea4a07dcb2d96e359a6f
tiagodsilva <tdsh97@gmail.com>
2022-06-13 01:48:52 -0300

    Update Celery's broker URL

--- taesb/celery/celery.py
+++ taesb/celery/celery.py
@@ -12,9 +12,10 @@ from typing import Dict
 # Instantiate a flask application 
 app = CelerySpark( 
         main="taesb", 
-        broker="amqp://", 
+        broker=\
+                "amqps://TJG:passwordpassword@b-30957be3-40c4-4c26-9193-8bcdead4625c.mq.us-east-1.amazonaws.com:5671",
         include=["taesb.celery.tasks"]
-) 
+)
 app.autodiscover_tasks() 
 
 if __name__ == "__main__": 
