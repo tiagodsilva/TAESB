76599a91d0dc8029c5a7ba7489ad23bb64e52692
tiagodsilva <tdsh97@gmail.com>
2022-06-16 23:19:25 -0300

    Update dashboad's design

--- spark_dash.py
+++ spark_dash.py
@@ -1,12 +1,13 @@
 # sys
 import dash
-from dash import dcc, html 
+from dash import dcc, html, dash_table 
 from dash.dependencies import Input, Output 
 import pandas as pd
 
 import plotly.graph_objs as go
 
 import psycopg2 
+import psycopg2.extras 
 
 # Capture credentials to access the data base 
 from taesb.SparkConf import POSTGRESQL_HOST, \
@@ -35,7 +36,8 @@ app.title = 'TAESB Dash with Spark'
 # Here we are creating the first plot, which will be located at left 
 plot_1 = html.Div(className="first-plot",
     children=[
-        dcc.Graph(
+        html.Div(
+            "Initial plot", 
             id="antsFood",
         ), 
         dcc.Interval( 
@@ -112,7 +114,9 @@ def execute_query(query: str):
     """ 
     global db_conn 
     # Instantiate cursor 
-    cursor = db_conn.cursor() 
+    cursor = db_conn.cursor(
+            cursor_factory=psycopg2.extras.RealDictCursor
+    ) 
     # Execute query 
     cursor.execute(query) 
     # Fetch instances 
@@ -128,10 +132,37 @@ def update_buttons(n_intervals: int):
     Update the buttons. 
     """ 
     # Capture the scnearios  
-    query = "SELECT scenario_id FROM scenarios;" 
+    query = "SELECT scenario_id FROM stats_local;" 
     scenarios = execute_query(query) 
     # Return the buttons 
-    return ["NULL"] + [identifier[0] for identifier in scenarios] 
+    return ["NULL"] + [row["scenario_id"] for row in scenarios] 
+
+@app.callback(Output("antsFood", "children"), 
+        [Input("antsStatsInterval", "n_intervals")]) 
+def update_global(n_intervals: int): 
+    """ 
+    Update global aspects about the simulation in the Dashboard. 
+    """ 
+    # Capture the data 
+    query = "SELECT * FROM stats_global;" 
+    stats = execute_query(query)[0] # Initial row 
+    
+    # Modify the format of the JSON: 
+    # instead of `key`: `value`, in which 
+    # `key` equals the column name in the database, 
+    # use 'keys': data.keys, 'values': data.values 
+    data = dict() 
+    items = [(key, stats[key]) for key in stats.keys()] 
+    data = [{
+        "Attributes": attr, 
+        "Values": val 
+    } for attr, val in items] 
+
+    return [dash_table.DataTable( 
+            data=data, 
+            columns=[{"name": i, "id": i} for i in data[1].keys()] 
+    )] 
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
