a0212638560b4287a4b80d2632452a3d30d24fe0
tiagodsilva <tdsh97@gmail.com>
2022-06-18 17:38:28 -0300

    Update class for benchmarks

--- taesb/utils/BenchmarkTask.py
+++ taesb/utils/BenchmarkTask.py
@@ -20,17 +20,25 @@ class BenchmarkTask(task):
     _start_time: int = None 
     _current_time: int = None 
     
-    def start_pipeline(self): 
+    _scenario_id: str = None 
+
+    def start_pipeline(self, scenario_id: str): 
         """ 
         Assert the start of the pipeline. 
         """ 
         self._start_time = time.time() # Persistent across processes 
-
-    def timedelta(self): 
+        
+        # Intrudoce the scenario's identifier as an attribute 
+        self._scenario_id = scenario_id 
+    
+    def timedelta(self, scenario_id: str): 
         """ 
         Compute the interval to execute the pipeline. 
         """ 
         self._current_time = time.time() 
+        
+        # Check the consistency of the scenario's identifier 
+        self._scenario_id = scenario_id 
 
         # Return the execution's interval 
         return self._current_time - self._start_time 
